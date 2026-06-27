031a73385ae2014217aa51432542a2cad47359fd
Riddhesh Patel <patel3y3r@gmail.com>
2026-06-28 02:39:21 +0530

    fix: force https URL scheme in production to resolve mixed content blocking of CSS/JS assets

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -22,6 +22,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if ($this->app->environment('production')) {
+            \URL::forceScheme('https');
+        }
+
         View::composer('*', function($view) {
             $view->with([
                 'globalSettings' => Setting::pluck('value', 'key'),
